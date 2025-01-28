cd441c4e04295980ccc2f2f1a4039fe6ec471d77
Habiba3698 <108907797+Habiba3698@users.noreply.github.com>
2025-01-28 02:44:46 +0200

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -155,14 +155,14 @@ elif status== 'Developing':
         st.markdown(f'''
             <div class="metric">
                 <h3>Average Life Expectancy</h3>
-                <p>{df_developing['Life expectancy'][0]}</p>
+                <p>{df_developing['Life expectancy'][1]}</p>
             </div>
         ''', unsafe_allow_html=True)
         st.write(" ")
         st.markdown(f'''
             <div class="metric">
                 <h3>Average Adult Mortality</h3>
-                <p>{df_developing['Adult Mortality'][0]}</p>
+                <p>{df_developing['Adult Mortality'][1]}</p>
             </div>
         ''', unsafe_allow_html=True)
         st.write(" ")
@@ -170,7 +170,7 @@ elif status== 'Developing':
         st.markdown(f'''
             <div class="metric">
                 <h3>Average GDP</h3>
-                <p>{df_developing['GDP'][0]}</p>
+                <p>{df_developing['GDP'][1]}</p>
             </div>
         ''', unsafe_allow_html=True)
 
@@ -180,7 +180,7 @@ elif status== 'Developing':
         st.markdown(f'''
             <div class="metric">
                 <h3>Average Population</h3>
-                <p>{df_developing['Population'][0]}</p>
+                <p>{df_developing['Population'][1]}</p>
             </div>
         ''', unsafe_allow_html=True)
 
@@ -189,7 +189,7 @@ elif status== 'Developing':
         st.markdown(f'''
             <div class="metric">
                 <h3>Avg BMI</h3>
-                <p>{df_developing['BMI'][0]}</p>
+                <p>{df_developing['BMI'][1]}</p>
             </div>
         ''', unsafe_allow_html=True)
         
@@ -198,7 +198,7 @@ elif status== 'Developing':
         st.markdown(f'''
             <div class="metric">
                 <h3>Average Schooling Years</h3>
-                <p>{df_developing['Schooling'][0]}</p>
+                <p>{df_developing['Schooling'][1]}</p>
             </div>
         ''', unsafe_allow_html=True)
 
